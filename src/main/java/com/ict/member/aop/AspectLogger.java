3ca7b4f6b7391c9de112db0a7a798b79727a8982
koyoungchan <skswkd2ek@naver.com>
2018-10-15 11:19:28 +0900

    영찬이 컴터 푸시

--- src/main/java/com/ict/member/aop/AspectLogger.java
+++ src/main/java/com/ict/member/aop/AspectLogger.java
@@ -1,7 +1,9 @@
 package com.ict.member.aop;
 
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Service;
@@ -18,12 +20,22 @@ public class AspectLogger {
 
 	private static final Logger logger = LoggerFactory.getLogger(AspectLogger.class);        
 	private long sTime;  
-	@Before("execution(* com.ict.member.Controller.URIController.*(..))")
+	@Before("execution(* com.ict.member.Controller.*.*(..))")
 	public void beforeLog(JoinPoint jp) {
 		logger.debug("jp=>{}",jp);//시작
 		sTime=System.currentTimeMillis();
 	}
-	@After("execution(* com.ict.member.Controller.URIController.*(..))")
+	@Around("execution(* com.ict.member.Controller.*.*(..))")
+	public Object aroundLog(ProceedingJoinPoint pjp) throws Throwable {
+		String sigName=pjp.getSignature().getName();
+		String targetName = pjp.getTarget().toString();
+		Object[] params =pjp.getArgs();
+		logger.debug("{}.{}({})",new Object[] {targetName,sigName,params});
+		Object obj = pjp.proceed();
+		logger.debug("result=>{}",obj);
+		return obj;	
+	}
+	@After("execution(* com.ict.member.Controller.*.*(..))")
 	public void afterLog(JoinPoint jp) {
 		logger.debug("end={}",jp);//끝날때 찍는것
 		logger.debug("execution time=>{}ms",(System.currentTimeMillis()-sTime));
